4335f9793faa92d2b18b808971ac588d23b8e9a8
Michał Szablewski <46929173+micszab@users.noreply.github.com>
2025-02-19 01:53:00 +0100

    Update to new amazon schema

--- crawler/amazon_crawler.rb
+++ crawler/amazon_crawler.rb
@@ -1,4 +1,4 @@
-#!/usr/bin/env ruby
+#!/usr/bin/ruby
 require 'nokogiri'
 require 'open-uri'
 require 'sequel'
@@ -18,7 +18,6 @@ def fetch_and_save_products(keyword)
   base_url = build_amazon_url(keyword)
   user_agent = user_agent_string
   doc = fetch_page(base_url, user_agent)
-
   process_product_cards(doc)
   puts 'Product data has been saved to the SQLite database.'
 end
@@ -36,28 +35,59 @@ def fetch_page(url, user_agent)
 end
 
 def process_product_cards(doc)
-  doc.css('div.s-main-slot div[data-component-type="s-search-result"]').each do |product_card|
+  doc.css('div[data-component-type="s-search-result"]').each do |product_card|
+    next if product_card.at_css('[aria-label="Sponsored"]')
+
     title = extract_title(product_card)
     price = extract_price(product_card)
     url = extract_url(product_card)
 
-    save_product(title, price, url) if price
+    # Only save if the product is valid (including URL check)
+    if valid_product?(title, price, url)
+      save_product(title, price, url)
+    else
+      puts "Skipping invalid product: #{title.inspect} - URL: #{url.inspect}"
+    end
   end
 end
 
+def valid_product?(title, price, url)
+  ![title, price, url].any?(&:nil?) && 
+  title != 'No title' && 
+  !url.to_s.empty? && 
+  price.to_f > 0 && 
+  !url.to_s.include?('/sspa/click') && 
+  !url.to_s.include?('/gp/slredirect/') && 
+  !url.to_s.include?('/ad/dp/')
+end
+
 def extract_title(product_card)
-  title_element = product_card.at_css('h2 a.a-link-normal span')
-  title_element ? title_element.text.strip : 'No title'
+  title_element = product_card.at_css('h2.a-size-medium span')
+
+  if title_element
+    title = title_element.text.strip
+    title = title.gsub(/\uFFFD/, '').strip
+    title = title.gsub(/Sponsored$/, '').strip 
+    return title unless title.empty?
+  end
+
+  return 'No title'
 end
 
 def extract_price(product_card)
-  price_whole = product_card.at_css('span.a-price-whole')&.text
-  price_fraction = product_card.at_css('span.a-price-fraction')&.text
-  price_whole && price_fraction ? "#{price_whole}.#{price_fraction}".to_f : nil
+  price_text = product_card.at_css('.a-price .a-offscreen')&.text
+  return nil unless price_text
+  
+  price_text.scan(/[\d\.]+/).join.to_f
 end
 
 def extract_url(product_card)
-  "https://www.amazon.com#{product_card.at_css('h2 a.a-link-normal')[:href]}"
+  link = product_card.at_css('h2 a.a-link-normal') || 
+         product_card.at_css('a.s-no-outline')
+  return nil unless link
+  
+  uri = link['href'].split('?').first
+  "https://www.amazon.com#{uri}"
 end
 
 def save_product(title, price, url)
